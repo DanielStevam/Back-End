a9ec35424b4a79dfabd781d6aeee50192f67d337
DanielStevam <danielestevam828@gmail.com>
2023-10-05 19:42:14 -0300

    update 2

---
+++ controllers/colaborador-controller.js
@@ -0,0 +1,12 @@
+const mysql = require("../mysql");
+
+exports.getColaborador = async (req, res, next) => {
+  try {
+    const response = {
+      message: "success",
+    };
+    return res.status(200).send(response);
+  } catch (error) {
+    return res.status(500).send({ error: error });
+  }
+};
